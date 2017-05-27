ff7f246aabe131dc5eec0b59f9c9d358803c9a11
Daniel Radetsky <dradetsky@gmail.com>
2017-05-27 13:18:19 -0700

    init

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+import './subtitle.js';
+import './syntax.js';
+// import './prism-syntax.js';
+
+// import 'bootstrap-loader';
+// import 'bootstrap-sass';
+import turbolinks from 'turbolinks';
+turbolinks.start();
+
+
---
+++ assets/js/subtitle.js
@@ -0,0 +1,25 @@
+import $ from 'jquery';
+import _ from 'lodash';
+const subtitle_options = [
+    'funrolling the rainbow',
+    'to the man(3r) born',
+    'how to make install like a porn star',
+    'all quiet on us-west-1',
+    'for a good time, call-with-current-continuation',
+    'a farewell to ARMs',
+    'tiers in heaven',
+    'putting the "meth" in "programming methodology"',
+    'free as in willy',
+    'build the -Wall',
+    'woke on lan',
+];
+
+const alternatives = [
+    'cat /dev/random | xxd > blogpost.html',
+    "(cons 8======D '())",
+];
+
+document.addEventListener("turbolinks:load", function() {
+    var new_subtitle = _.sample(subtitle_options);
+    $('#subtitle').text(new_subtitle);
+});
